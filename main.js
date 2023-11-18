ee278dc037560bf49b0a88854b956c2bea163546
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2023-11-18 12:51:50 +0100

    Add end section

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ import Reveal from 'reveal.js';
 Reveal.initialize({
     autoPlayMedia: true,
     disableLayout: false,
-    navigationMode: "linear"
+    navigationMode: "linear",
+    hash: true,
 })
